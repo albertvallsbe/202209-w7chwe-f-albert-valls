5d4725023757f6fb16cdf9c3da1abebf86394f96
Albert Valls <avalls89@gmail.com>
2022-11-14 23:22:19 +0100

    Add register in home page

---
+++ src/components/Forms/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import useUser from "../../../hooks/useUser";
+import { UserCredentials } from "../../../types/types";
+import Button from "../../Button/Button";
+import { FormStyled } from "../FormStyled";
+
+const RegisterForm = (): JSX.Element => {
+  const { login } = useUser();
+
+  const initialFormData = {
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleFormChange = (
+    event:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+
+    const user: UserCredentials = {
+      username: formData.username,
+      password: formData.password,
+    };
+    login(user);
+  };
+
+  return (
+    <>
+      <FormStyled onSubmit={handleSubmit}>
+        <div className="form__container">
+          <div className="form__block">
+            <div className="form__group">
+              <label className="form__label" htmlFor="username">
+                Username
+              </label>
+              <input
+                type="text"
+                name="userName"
+                id="username"
+                onChange={handleFormChange}
+                autoComplete="off"
+                required
+              />
+            </div>
+            <div className="form__group">
+              <label className="form__label" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleFormChange}
+                autoComplete="off"
+                required
+              />
+            </div>
+            <div className="form__group">
+              <label className="form__label" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={handleFormChange}
+                autoComplete="off"
+                required
+              />
+            </div>
+          </div>
+          <Button action={() => {}} text="Register" />
+          <p className="form__link">
+            Registered? <a href="/login">Login now</a>
+          </p>
+        </div>
+      </FormStyled>
+    </>
+  );
+};
+
+export default RegisterForm;
