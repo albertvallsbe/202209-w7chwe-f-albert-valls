73c06c77e6e4740ac5dd82e56517b27e3810017a
Albert Valls <avalls89@gmail.com>
2022-11-16 00:10:52 +0100

    Add logo to site

--- src/components/HelloComponent/HelloComponent.tsx
+++ src/components/HelloComponent/HelloComponent.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import FormStyled from "../Forms/FormStyled";
+import LogoComponent from "../LogoComponent/LogoComponent";
 
 const HelloComponent = (): JSX.Element => {
   return (
     <FormStyled>
       <div className="form__container">
+        <LogoComponent />
         <div className="form__block">
           <Link to="/users/register">
             <Button action={() => {}} text="Register" />
---
+++ src/components/LogoComponent/LogoStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LogoStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 0;
+  padding-bottom: -2rem;
+  justify-content: center;
+`;
+
+export default LogoStyled;
